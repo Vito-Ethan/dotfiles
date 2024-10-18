6c9cf53610cc151e9ed351ecedee1dca6c42eac9
Vito-Ethan <evito@unr.edu>
2024-10-18 00:08:36 -0700

    Add keybind for gitsigns hunk preview

--- lua/custom/plugins/gitsigns.lua
+++ lua/custom/plugins/gitsigns.lua
@@ -6,14 +6,19 @@ return {
   -- See `:help gitsigns` to understand what the configuration keys do
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = 'A' },
-        change = { text = 'M' },
-        delete = { text = 'D' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
+    config = function()
+      local gitsigns = require 'gitsigns'
+
+      gitsigns.setup {
+        signs = {
+          add = { text = 'A' },
+          change = { text = 'M' },
+          delete = { text = 'D' },
+          topdelete = { text = '‾' },
+          changedelete = { text = '~' },
+        },
+      }
+      vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', { desc = '[G]it [P]review hunk' })
+    end,
   },
 }
