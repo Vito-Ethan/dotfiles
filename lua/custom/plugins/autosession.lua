3fe4a2b86ff9209a5591d0c0ea7057f8f4d57158
Vito-Ethan <evito@unr.edu>
2024-11-24 00:27:37 -0800

    Modify key shortcuts
    Use Autosession delete instead of SessionDelete since the former brings
    up a telescope window of saved sesssions.

--- lua/custom/plugins/autosession.lua
+++ lua/custom/plugins/autosession.lua
@@ -3,10 +3,10 @@ return {
     'rmagatti/auto-session',
     lazy = false,
     keys = {
-      { '<leader>ls', '<cmd>SessionSearch<CR>', desc = 'Session search' },
-      { '<leader>lw', '<cmd>SessionSave<CR>', desc = 'Save session' },
+      { '<leader>ls', '<cmd>SessionSearch<CR>', desc = 'Search sessions' },
+      { '<leader>lw', '<cmd>SessionSave<CR>', desc = 'Save cwd session' },
       { '<leader>la', '<cmd>SessionToggleAutoSave<CR>', desc = 'Toggle autosave' },
-      { '<leader>ld', '<cmd>SessionDelete<CR>', desc = 'Delete CWD Session' },
+      { '<leader>ld', '<cmd>Autosession delete<CR>', desc = 'Choose a session to delete' },
     },
     ---enables autocomplete for opts
     ---@module "auto-session"
