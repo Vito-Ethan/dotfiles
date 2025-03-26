20d469e3c2915397724883060fe6d56399a79645
Vito-Ethan <evito@unr.edu>
2025-03-26 12:38:28 -0700

    feat: update lualine sections

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -10,10 +10,20 @@ return {
         },
         sections = {
           lualine_a = { 'mode' },
-          lualine_b = { { 'filename', path = 4 }, 'branch', 'diff', 'diagnostics' },
+          lualine_b = {
+            {
+              'filename',
+              path = 4,
+              symbols = {
+                modified = '[M]', -- Text to show when the file is modified.
+                readonly = '[🔒]', -- Text to show when the file is non-modifiable or readonly.
+              },
+            },
+            'branch',
+          },
           lualine_c = { 'buffers' },
-          lualine_x = { 'encoding', 'fileformat', 'filetype' },
-          lualine_y = { 'progress' },
+          lualine_x = {},
+          lualine_y = { 'filetype' },
           -- NOTE: The style formats the os.date similar to printf in C
           -- Uses the strftime format
           lualine_z = { { 'datetime', style = '%a %b %d %H:%M' } },
