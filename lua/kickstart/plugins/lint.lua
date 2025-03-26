adcab02ab7d182f013e23ff24598714ed2f15a26
Vito-Ethan <evito@unr.edu>
2025-03-26 12:38:35 -0700

    feat: add linter for go

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -14,6 +14,7 @@ return {
         python = { 'pylint' },
         html = { 'htmlhint' },
         css = { 'stylelint' },
+        go = { 'golangci-lint' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
